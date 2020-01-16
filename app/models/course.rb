8282d228bad7b718f7473bf040637ed66383605f
Jan Vacek <honza394@gmail.com>
2020-01-16 21:56:56 +0100

    index courses view with partials

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,4 +1,6 @@
 class Course < ApplicationRecord
-
+  validates :short_name, presence: true
+  validates :name, presence: true, length: { minimum: 3, maximum: 30 }
+  validates :description, presence: true, length: { minimum: 10 }
 
 end
