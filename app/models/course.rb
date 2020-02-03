52b64abb6a4fda75f92d5acaa93b2ac3a8296c44
Jan Vacek <honza394@gmail.com>
2020-02-03 09:01:36 +0100

    many to many associations

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,5 +2,6 @@ class Course < ApplicationRecord
   validates :short_name, presence: true
   validates :name, presence: true, length: { minimum: 3, maximum: 30 }
   validates :description, presence: true, length: { minimum: 10 }
-
+  has_many :student_courses
+  has_many :students, through: :student_courses
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'courses#index'
   get 'courses/new', to: 'courses#new'
+  get 'courses/:id', to: 'courses#show', as: 'course'
   get 'about', to: 'pages#about'
 
   resources 'students', except: [:destroy]
@@ -8,4 +9,5 @@ Rails.application.routes.draw do
   get 'login', to: 'logins#new'
   post 'login', to: 'logins#create'
   delete 'logout', to: 'logins#destroy'
+  post 'course_enroll', to: 'student_courses#create'
 end
